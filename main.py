73c9a20b623a21bbc3463a52d363e77ed35a33bf
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-21 01:24:05 +0300

    project is finished.

---
+++ main.py
@@ -0,0 +1,37 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("TURKEY STATES GAME")
+screen.bgcolor("pink")
+screen.setup(width= 1400, height= 600)
+image = "tr_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("tr_illeri.csv")
+all_states = data.il.to_list()
+guessed_states = []
+
+while len(guessed_states) != len(all_states):
+    answer_state = screen.textinput(title = f"{len(guessed_states)} /81 States Correct", prompt = "What's another state's name?").capitalize()
+
+    if answer_state == "Exit":
+        missing_states =[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.il == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+screen.exitonclick()
